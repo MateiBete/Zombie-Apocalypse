25ccbfb0b0b54ef90b7917d209009df6d7dcd618
MateiBete <matei.beteringhe@gmail.com>
2024-02-02 20:20:00 +0200

    Animator

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
+    float horizontalMove = 0f;
+    public Animator animator;
     public float Speed = 1000f;
     public Rigidbody2D rb;
     public GameObject YouDied;
@@ -20,19 +22,34 @@ public class PlayerMovement : MonoBehaviour
         YouDied.SetActive(true);
         Destroy(gameObject);
     }
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     void Update()
     {
-        if (Input.GetKey("w"))
+        if (Input.GetKey("w")){
             rb.AddForce(Vector2.up * Speed * Time.deltaTime);
-
-        if (Input.GetKey("s"))
+            animator.SetBool("IsMoving", true );
+        }
+        if (Input.GetKey("s")){
             rb.AddForce(Vector2.down * Speed * Time.deltaTime);
-
-        if (Input.GetKey("d"))
+            animator.SetBool("IsMoving", true );
+        }
+        if (Input.GetKey("d")){
             rb.AddForce(Vector2.right * Speed * Time.deltaTime);
-
-        if (Input.GetKey("a"))
+            animator.SetBool("IsMoving", true );
+        }
+        else{
+            animator.SetBool("IsMoving", false);
+        }
+        if (Input.GetKey("a")){
             rb.AddForce(Vector2.left * Speed * Time.deltaTime);
+            animator.SetBool("IsMoving", true );
+        }
+        else{
+            animator.SetBool("IsMoving", false);
+        }
+            
     }
 }
